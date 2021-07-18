ef97d4bce68461747be37dfd161b4ae71eae12d2
akbarmarunduri <akbarmarunduri@gmail.com>
2021-07-18 19:44:55 +0700

    memulai belajar spring boot

---
+++ spring-design-patter-creational/src/main/java/amd/programmer/java/spring/creational/SpringDesignPatterCreationalApplication.java
@@ -0,0 +1,13 @@
+package amd.programmer.java.spring.creational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesignPatterCreationalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDesignPatterCreationalApplication.class, args);
+	}
+
+}
